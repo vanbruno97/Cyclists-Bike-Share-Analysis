897a67d8457a248c8ce9eadd6f00766ede704b72
vanbruno97 <127063799+vanbruno97@users.noreply.github.com>
2026-07-30 22:40:07 +0200

    Add null value counts for trip data fields
    Added a query to count null values for various fields in the trip data.

--- sql/data_exploration.sql
+++ sql/data_exploration.sql
@@ -39,6 +39,39 @@ COUNTIF(end_station_id IS NULL) AS end_station_id_null
 FROM `project-6577c817-95ba-41b4-a38.trip_data.tripdata_01`;
 
 
+--final visualization of all the null values
+SELECT
+    COUNT(*) AS total_rows,
+
+    COUNTIF(ride_id IS NULL) AS ride_id_null,
+
+    COUNTIF(rideable_type IS NULL) AS rideable_type_null,
+
+    COUNTIF(started_at IS NULL) AS started_at_null,
+
+    COUNTIF(ended_at IS NULL) AS ended_at_null,
+
+    COUNTIF(start_station_name IS NULL) AS start_station_name_null,
+
+    COUNTIF(start_station_id IS NULL) AS start_station_id_null,
+
+    COUNTIF(end_station_name IS NULL) AS end_station_name_null,
+
+    COUNTIF(end_station_id IS NULL) AS end_station_id_null,
+
+    COUNTIF(start_lat IS NULL) AS start_lat_null,
+
+    COUNTIF(start_lng IS NULL) AS start_lng_null,
+
+    COUNTIF(end_lat IS NULL) AS end_lat_null,
+
+    COUNTIF(end_lng IS NULL) AS end_lng_null,
+
+    COUNTIF(member_casual IS NULL) AS member_casual_null
+
+FROM `project-6577c817-95ba-41b4-a38.trip_data.all_trips_2025`;
+
+
 
 --visualization of all the duplicates
 SELECT
